075eecad0af94696cf280977e7147b8db0b360d3
j_dalrym2 <j_dalrym2@hotmail.com>
2022-05-18 23:11:48 -0600

    Added class to describe WWA product from VTEC format

---
+++ nwstools/fetchwwa/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pathlib
+
+from .. import get_logger as _get_logger
+from .. import get_download_dir as _get_download_dir
+
+
+def get_logger():
+    return _get_logger()
+
+
+def get_download_dir():
+    d = pathlib.Path(_get_download_dir(), 'mrms')
+    d.mkdir(parents=False, exist_ok=True)
+    return d
+
+
+from .wwa_polygon import WWAPolygon
